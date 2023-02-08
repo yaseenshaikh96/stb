004e38c6a885e188bd0125f821480f3179751535
Yaseen <shaikhyaseen6496@gmail.com>
2023-02-08 15:19:58 +0530

    init

---
+++ premake5.lua
@@ -0,0 +1,23 @@
+project "stb"
+	kind "None"
+	language "C"
+
+	files
+	{
+		"src/**.h",
+		"src/**.c",
+		"src/**.cpp"
+	}
+
+	filter "configurations:Debug"
+		runtime "Debug"
+		symbols "on"
+
+	filter "configurations:Release"
+		runtime "Release"
+		optimize "on"
+
+	filter "configurations:Dist"
+		runtime "Release"
+		optimize "on"
+        symbols "off"
